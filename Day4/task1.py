3e48849593b80348086961bde7fcaca361c6ec00
Azad Singh <118439188+Ajjurathore@users.noreply.github.com>
2026-03-16 11:58:25 +0530

    Document tasks for Selenium script in task1.py
    Added comments outlining the tasks for the Selenium script.

--- Day4/task1.py
+++ Day4/task1.py
@@ -1,3 +1,16 @@
+## Task 1
+
+### CSS Locators
+
+Write a Selenium script for the following:
+
+1. Go to `https://the-internet.herokuapp.com/login`
+2. Locate the username field using CSS Selector with tag and `name` attribute.
+3. Locate the password field using CSS Selector with tag and `id` attribute.
+4. Locate the **Login** button using CSS Selector with tag and `type` attribute.
+5. Locate the footer link **Elemental Selenium** using CSS Selector with descendant selector.
+
+---
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
